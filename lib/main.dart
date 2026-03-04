d284f1e77240e0ee2170ae8c6fc2bb17e4830496
Luiz CD Prof <luizcdprof@gmail.com>
2026-03-04 20:17:53 -0300

    version 01 - simple and colorless

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() => runApp(MaterialApp(home: TodoList()));
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  TodoListState createState() => TodoListState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class TodoListState extends State<TodoList> {
+  final TextEditingController _controller = TextEditingController();
+  List<String> tarefas = [];
+
+  void _adicionarTarefa() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        tarefas.add(_controller.text);
+      });
+      _controller.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Minha Lista de Tarefas')),
+      body: ListView.builder(
+        itemCount: tarefas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tarefas[index]),
+            trailing: IconButton(
+              icon: Icon(Icons.delete, color: Colors.red),
+              onPressed: () => setState(() => tarefas.removeAt(index)),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(child: TextField(controller: _controller)),
+            IconButton(icon: Icon(Icons.add), onPressed: _adicionarTarefa),
+          ],
         ),
       ),
     );
